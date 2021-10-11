ac2e7ea3e4207db56d261b48e6b88d022bd0a41b
Progrema Studio <iqbalpakeh@gmail.com>
2021-10-12 06:05:00 +0700

    Implement list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -17,4 +18,10 @@ export class HeroesComponent implements OnInit {
   }
 
   heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
