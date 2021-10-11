27b2c57ccc93a9c2f1b06b3a23bce05989e7b97d
Progrema Studio <iqbalpakeh@gmail.com>
2021-10-12 05:51:03 +0700

    Add hero lists

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +8,13 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  constructor() {
+    //
+  }
 
-  constructor() {}
+  ngOnInit(): void {
+    //
+  }
 
-  ngOnInit(): void {}
+  heroes = HEROES;
 }
